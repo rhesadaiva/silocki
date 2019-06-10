ec2a080f28b3ba84ea8cab4af12ef0a276806d83
rhesadaiva <rhesa.daiva@gmail.com>
2019-06-10 22:59:03 +0700

    mengganti sidebar dari approval ke persetujuan atasan

--- application/views/templates/sidebar_admin.php
+++ application/views/templates/sidebar_admin.php
@@ -55,11 +55,11 @@
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-fw fa-check"></i>
-            <span>Approval</span>
+            <span>Persetujuan Atasan</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item mx-auto" href="<?= base_url('pejabat/kontrakkinerjabawahan'); ?>">Approval Kontrak Kinerja</a>
+                <a class="collapse-item mx auto" href="<?= base_url('pejabat/kontrakkinerjabawahan'); ?>">Approval Bawahan</a>
             </div>
         </div>
     </li>
