07c6aef647cc47d6f4fc201be9e405031074433a
rhesadaiva <rhesa.daiva@gmail.com>
2019-06-14 09:53:09 +0700

    minor update

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -60,3 +60,22 @@
 
         </nav>
         <!-- End of Topbar -->
+
+        <!-- Logout Modal-->
+        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Konfirmasi Logout</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">Apakah anda yakin akan logout?</div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                        <a class="btn btn-primary" href="<?= base_url('auth/logout'); ?>">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </div>
