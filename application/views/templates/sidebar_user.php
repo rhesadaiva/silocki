58f4fed17f0da4a581646b1cd26cf00120408ee5
rhesadaiva <rhesa.daiva@gmail.com>
2019-06-17 17:18:30 +0700

    memperbaiki sidebar links

--- application/views/templates/sidebar_user.php
+++ application/views/templates/sidebar_user.php
@@ -2,19 +2,24 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
   <!-- Sidebar - Brand -->
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('pelaksana') ?>">
     <div class="sidebar-brand-icon rotate-n-15">
       <i class="fas fa-fw fa-book"></i>
     </div>
-    <div class="sidebar-brand-text mx-3">E-Logbook</div>
+    <div class="sidebar-brand-text mx-3">SILOCKI</div>
   </a>
 
   <!-- Divider -->
-  <hr class="sidebar-divider my-0">
+  <hr class="sidebar-divider">
+
+  <!-- Heading -->
+  <div class="sidebar-heading">
+    Home
+  </div>
 
   <!-- Nav Item - Dashboard -->
   <li class="nav-item">
-    <a class="nav-link" href="index.html">
+    <a class="nav-link" href="<?= base_url('pelaksana') ?>">
       <i class="fas fa-fw fa-tachometer-alt"></i>
       <span>Dashboard</span></a>
   </li>
@@ -41,30 +46,14 @@
     </div>
   </li>
 
-  <!-- Nav Item - Utilities Collapse Menu -->
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-      <i class="fas fa-fw fa-wrench"></i>
-      <span>Utilities</span>
-    </a>
-    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Custom Utilities:</h6>
-        <a class="collapse-item" href="utilities-color.html">Colors</a>
-        <a class="collapse-item" href="utilities-border.html">Borders</a>
-        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-        <a class="collapse-item" href="utilities-other.html">Other</a>
-      </div>
-    </div>
-  </li>
+
 
   <!-- Divider -->
   <hr class="sidebar-divider">
 
   <!-- Nav Item - Pages Collapse Menu -->
 
-  <!-- Divider -->
-  <hr class="sidebar-divider d-none d-md-block">
+
 
   <!-- Sidebar Toggler (Sidebar) -->
   <div class="text-center d-none d-md-inline">
