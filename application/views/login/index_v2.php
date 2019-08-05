e55c5af5aaccba0f01b50a80871904d24ecd952b
rhesadaiva <rhesa.daiva@gmail.com>
2019-08-05 11:14:02 +0700

    menambah halaman login baru

--- application/views/login/index_v2.php
+++ application/views/login/index_v2.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="shortcut icon" href="<?= base_url() ?>assets/img/favicon.ico">
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>
         SiLoCKi KPPBC Tanjungpinang | Login
@@ -57,7 +57,7 @@
                                             <i class="material-icons">lock_outline</i>
                                         </span>
                                     </div>
-                                    <input type="password" name="password" class="form-control" placeholder="Password">
+                                    <input type="password" name="password" class="form-control" placeholder="Password" autocomplete off>
                                     <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                 </div>
 
@@ -85,13 +85,13 @@
     <script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
     <script src="assets/js/core/popper.min.js" type="text/javascript"></script>
     <script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-    <script src="<?= base_url() ?>assets/js/plugins/moment.min.js"></script>
+    <script src="assets/js/plugins/moment.min.js"></script>
     <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
-    <script src="<?= base_url() ?>assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+    <script src="assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
     <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-    <script src="<?= base_url() ?>assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+    <script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
     <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-    <script src="<?= base_url() ?>assets/js/material-kit.min.js?v=2.0.5" type="text/javascript"></script>
+    <script src="assets/js/material-kit.min.js?v=2.0.5" type="text/javascript"></script>
 
 </body>
 
