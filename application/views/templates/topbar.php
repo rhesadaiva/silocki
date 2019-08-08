0b9adb4ec35cdf0f48ed6b87ce5339562e3913b9
rhesadaiva <rhesa.daiva@gmail.com>
2019-08-08 17:40:55 +0700

    menambah title di topbar

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -6,6 +6,10 @@
 
         <!-- Topbar -->
         <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+            <div class="application-name">
+                <h6 class="application-name"><b>Sistem Laporan Capaian Kinerja KPPBC Tanjungpinang</b></h6>
+                <h6>Versi: 1.0</h6>
+            </div>
 
             <!-- Sidebar Toggle (Topbar) -->
             <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
