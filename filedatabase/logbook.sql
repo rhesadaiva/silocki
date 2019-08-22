a0c958a1db53ec32dcfeab5ec47df0f47c16c7ca
rhesadaiva <rhesa.daiva@gmail.com>
2019-08-22 20:40:32 +0700

    database baru dengan pengumuman dan konversi 120

--- filedatabase/logbook.sql
+++ filedatabase/logbook.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 13, 2019 at 12:45 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.1
+-- Host: localhost
+-- Generation Time: Aug 22, 2019 at 01:39 PM
+-- Server version: 8.0.15
+-- PHP Version: 7.1.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,18 +32,19 @@ CREATE TABLE `indikatorkinerjautama` (
   `id_iku` varchar(128) NOT NULL,
   `nip` varchar(128) NOT NULL,
   `nomorkk` varchar(128) NOT NULL,
-  `kodeiku` varchar(128) NOT NULL,
-  `namaiku` varchar(128) NOT NULL,
-  `formulaiku` text NOT NULL,
+  `kodeiku` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `namaiku` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `formulaiku` varchar(512) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `targetiku` varchar(128) NOT NULL,
   `nilaitertinggi` varchar(128) NOT NULL,
+  `satuanpengukuran` enum('Persentase','Indeks','Satuan','Waktu') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `aspektarget` enum('Kuantitas','Kualitas','Waktu','Biaya') NOT NULL,
-  `penanggungjawab` varchar(100) NOT NULL,
-  `penyediadata` varchar(100) NOT NULL,
+  `penanggungjawab` varchar(256) NOT NULL,
+  `penyediadata` varchar(256) NOT NULL,
   `sumberdata` varchar(100) NOT NULL,
-  `satuanpengukuran` varchar(100) NOT NULL,
-  `konsolidasiperiodeiku` enum('Sum','Average','Take Last Known','') NOT NULL,
   `periodepelaporan` enum('Bulanan','Triwulanan','Semesteran','Tahunan') NOT NULL,
+  `konsolidasiperiodeiku` enum('Sum','Average','Take Last Known','') NOT NULL,
+  `konversi120` enum('Ya','Tidak') NOT NULL,
   `iku_validated` int(12) NOT NULL,
   `nama_validated` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -58,7 +59,7 @@ CREATE TABLE `kontrakkinerja` (
   `id` varchar(128) NOT NULL,
   `nip` varchar(128) NOT NULL,
   `kontrakkinerjake` varchar(128) NOT NULL,
-  `nomorkk` varchar(128) NOT NULL,
+  `nomorkk` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `tanggalmulai` date NOT NULL,
   `tanggalselesai` date NOT NULL,
   `is_validated` int(11) NOT NULL,
@@ -75,17 +76,17 @@ CREATE TABLE `kontrakkinerja` (
 CREATE TABLE `logbook` (
   `id_logbook` varchar(225) NOT NULL,
   `id_iku` varchar(128) NOT NULL,
-  `nippegawai` varchar(128) NOT NULL,
-  `periode` varchar(255) NOT NULL,
+  `nippegawai` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `periode` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `perhitungan` varchar(255) NOT NULL,
-  `realisasibulan` varchar(255) NOT NULL,
-  `realisasiterakhir` varchar(255) NOT NULL,
-  `ket` text NOT NULL,
-  `wakturekam` varchar(255) NOT NULL,
+  `realisasibulan` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `realisasiterakhir` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `ket` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `wakturekam` datetime NOT NULL,
   `is_approved` int(2) NOT NULL,
   `is_sent` int(2) NOT NULL,
-  `tgl_approve` varchar(128) NOT NULL,
-  `tgl_batalapprove` varchar(128) NOT NULL,
+  `tgl_approve` datetime NOT NULL,
+  `tgl_batalapprove` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `nama_validated` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -122,12 +123,25 @@ INSERT INTO `pangkat` (`id`, `pangkat/golongan`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pengumuman`
+--
+
+CREATE TABLE `pengumuman` (
+  `id` int(11) NOT NULL,
+  `content` varchar(512) NOT NULL,
+  `tglrekam` date NOT NULL,
+  `is_sent` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `seksi/subseksi`
 --
 
 CREATE TABLE `seksi/subseksi` (
   `id` int(11) NOT NULL,
-  `seksi/subseksi` varchar(128) NOT NULL
+  `seksi/subseksi` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -184,7 +198,7 @@ INSERT INTO `seksi/subseksi` (`id`, `seksi/subseksi`) VALUES
 
 CREATE TABLE `tabel_log` (
   `log_id` int(11) NOT NULL,
-  `log_time` varchar(255) DEFAULT NULL,
+  `log_time` varchar(100) NOT NULL,
   `log_user` varchar(255) DEFAULT NULL,
   `log_tipe` int(11) DEFAULT NULL,
   `log_desc` varchar(255) DEFAULT NULL
@@ -199,24 +213,25 @@ CREATE TABLE `tabel_log` (
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `nama` varchar(128) NOT NULL,
-  `nip` varchar(128) NOT NULL,
+  `nip` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `pangkat` varchar(128) NOT NULL,
   `password` varchar(128) NOT NULL,
   `role_id` int(11) NOT NULL,
   `seksi` varchar(128) NOT NULL,
-  `atasan` varchar(128) NOT NULL
+  `atasan` varchar(128) NOT NULL,
+  `telegram` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `nama`, `nip`, `pangkat`, `password`, `role_id`, `seksi`, `atasan`) VALUES
-(1, 'Administrator', 'admin', 'Pengatur Muda Tk.I/II.b', '21232f297a57a5a743894a0e4a801fc3', 1, 'Seksi Pengolahan Data dan Administrasi Dokumen', 'Zuhalta'),
-(3, 'Kepala Kantor', 'kakan', 'Pembina/IV.a', '57aee06784fb57b06e11b4633ceeb9c3', 1, 'Kepala Kantor', 'Kepala Kantor'),
-(4, 'Rhesa Daiva Bremana', '199506072015021003', 'Pengatur Muda Tk.I /II.b', 'e10adc3949ba59abbe56e057f20f883e', 5, 'Pengolahan Data dan Administrasi Dokumen', 'Zuhalta'),
-(9, 'Zuhalta', '196212241983031001', 'Penata Tk.I/III.d', 'e10adc3949ba59abbe56e057f20f883e', 3, 'Seksi Pengolahan Data dan Administrasi Dokumen', 'Kepala Kantor'),
-(10, 'Sukardi', '196510102005011001', 'Pengatur Tk.I/II.d', 'e10adc3949ba59abbe56e057f20f883e', 5, 'Seksi Pengolahan Data dan Administrasi Dokumen', 'Zuhalta');
+INSERT INTO `user` (`id`, `nama`, `nip`, `pangkat`, `password`, `role_id`, `seksi`, `atasan`, `telegram`) VALUES
+(1, 'Administrator', 'admin', 'Pengatur Muda Tk.I/II.b', '21232f297a57a5a743894a0e4a801fc3', 1, 'Seksi Pengolahan Data dan Administrasi Dokumen', 'Zuhalta', '287035103'),
+(3, 'Kepala Kantor', 'kakan', 'Pembina/IV.a', '57aee06784fb57b06e11b4633ceeb9c3', 1, 'Kepala Kantor', 'Kepala Kantor', ''),
+(4, 'Rhesa Daiva Bremana', '199506072015021003', 'Pengatur Muda/II.a', '0ca4503d3a2257ea26cbbcc2651ecb4c', 5, 'Seksi Pengolahan Data dan Administrasi Dokumen', 'Zuhalta', '287035103'),
+(9, 'Zuhalta', '196212241983031001', 'Penata Tk.I/III.d', 'e10adc3949ba59abbe56e057f20f883e', 3, 'Seksi Pengolahan Data dan Administrasi Dokumen', 'Kepala Kantor', ''),
+(10, 'Sukardi', '196510102005011001', 'Pengatur Tk.I/II.d', 'e10adc3949ba59abbe56e057f20f883e', 5, 'Seksi Pengolahan Data dan Administrasi Dokumen', 'Zuhalta', '');
 
 -- --------------------------------------------------------
 
@@ -239,6 +254,21 @@ INSERT INTO `user_role` (`id`, `level`) VALUES
 (3, 'Kepala Seksi'),
 (4, 'Kepala Sub Seksi'),
 (5, 'Pelaksana');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_token`
+--
+
+CREATE TABLE `user_token` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nip` varchar(100) NOT NULL,
+  `token` varchar(100) NOT NULL,
+  `is_used` int(2) NOT NULL,
+  `is_sent` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -269,6 +299,12 @@ ALTER TABLE `pangkat`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `seksi/subseksi`
 --
 ALTER TABLE `seksi/subseksi`
@@ -293,6 +329,12 @@ ALTER TABLE `user_role`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user_token`
+--
+ALTER TABLE `user_token`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -301,6 +343,12 @@ ALTER TABLE `user_role`
 --
 ALTER TABLE `pangkat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `seksi/subseksi`
@@ -312,19 +360,25 @@ ALTER TABLE `seksi/subseksi`
 -- AUTO_INCREMENT for table `tabel_log`
 --
 ALTER TABLE `tabel_log`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user_token`
+--
+ALTER TABLE `user_token`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
