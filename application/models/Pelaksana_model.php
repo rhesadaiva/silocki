b954c024e8328231019b67128d78466a58c88a90
rhesadaiva <rhesa.daiva@gmail.com>
2019-09-24 17:47:28 +0700

    merubah role jadi nip

--- application/models/Pelaksana_model.php
+++ application/models/Pelaksana_model.php
@@ -5,9 +5,9 @@ class Pelaksana_model extends CI_Model
 {
     public function countKKApproved()
     {
-        $role = $this->session->userdata('nama');
+        $role = $this->session->userdata('nip');
 
-        $query = $this->db->query("SELECT `kontrakkinerja`.*, `user`.nama, `user`.`atasan` from kontrakkinerja  join user using (nip) where nama = '$role' and is_validated = 2");
+        $query = $this->db->query("SELECT `kontrakkinerja`.*, `user`.nama, `user`.nip, `user`.`atasan` from kontrakkinerja join user using (nip) where nip = '$role' and is_validated = 2");
         if ($query->num_rows() > 0) {
             return $query->num_rows();
         } else {
@@ -17,9 +17,9 @@ class Pelaksana_model extends CI_Model
 
     public function countIKUApproved()
     {
-        $role = $this->session->userdata('nama');
+        $role = $this->session->userdata('nip');
 
-        $query = $this->db->query("SELECT `indikatorkinerjautama`.*, `user`.nama, `user`.atasan from indikatorkinerjautama join user using (nip) where nama = '$role' and iku_validated = 1");
+        $query = $this->db->query("SELECT `indikatorkinerjautama`.*, `user`.nama, `user`.nip, `user`.atasan from indikatorkinerjautama join user using (nip) where user.nip = '$role' and iku_validated = 1");
         if ($query->num_rows() > 0) {
             return $query->num_rows();
         } else {
@@ -29,12 +29,12 @@ class Pelaksana_model extends CI_Model
 
     public function countLogbookSent()
     {
-        $role = $this->session->userdata('nama');
+        $role = $this->session->userdata('nip');
 
         $query = $this->db->query("SELECT `user`.`nama`, `user`.`nip`, `user`.`atasan`, `kontrakkinerja`.`nomorkk`, `kontrakkinerja`.`nip`, `indikatorkinerjautama`.id_iku, `indikatorkinerjautama`.`nip`, `logbook`.* 
                                     from `kontrakkinerja` right join `user` using (`nip`) 
                                     join `indikatorkinerjautama` using (`nomorkk`)
-                                    join `logbook` using (`id_iku`) where `is_sent` = 1 and `nama` = '$role'");
+                                    join `logbook` using (`id_iku`) where `is_sent` = 1 and `user`.nip = '$role' ");
         if ($query->num_rows() > 0) {
             return $query->num_rows();
         } else {
